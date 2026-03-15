83a65372f28bff5a7919851435041e576ece99e4
Pravin-Suthar <pravinsuthar693@gmail.com>
2026-03-15 22:21:03 +0530

    release: v1.0.0

--- src/s3cache/__init__.py
+++ src/s3cache/__init__.py
@@ -2,5 +2,5 @@ from s3cache.decorator import cached
 from s3cache.manager import CacheManager
 from s3cache.storage import LocalDiskBackend, S3Backend, StorageBackend
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __all__ = ["cached", "CacheManager", "StorageBackend", "S3Backend", "LocalDiskBackend"]
